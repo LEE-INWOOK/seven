1509ce23c3efbe64579353dec58dab87c1fefbb1
opera <opera@DESKTOP-14N4OAJ>
2021-05-10 16:43:45 +0900

    git ignore test

--- seven/src/main/java/com/seven/controller/AdminController.java
+++ seven/src/main/java/com/seven/controller/AdminController.java
@@ -86,11 +86,12 @@ public class AdminController {
 		productBean.setProduct_detail_del_info(request.getParameter("product_detail_del_info"));
 		productBean.setProduct_detail_sale(request.getParameter("product_detail_sale"));
 		productBean.setProduct_detail_admin_note(request.getParameter("product_detail_admin_note"));
+		System.out.println("임시");
 		
 		productService.insertProduct(productBean);
 		return "redirect:/product/shop";
 	}
-	// 상품등록 끝 ↑↑
+	// 상품등록 끝 asdasd
 	
 	
 
